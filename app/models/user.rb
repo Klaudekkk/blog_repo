6added5be0ccfccfca3be66dd49ab7710c11df75
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-08 16:49:07 +0200

    save email in downcase in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase} # self odnosi się do każdego obiektu klasy User
   has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
