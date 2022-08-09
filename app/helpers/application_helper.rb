e0ae77eee74d779110253042e0e895bdc11ed317
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-09 13:12:00 +0200

    User img profile, show controller in user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, option = {size:80})
+    email_adress = user.email
+    hash = Digest::MD5.hexdigest(email_adress)
+    size = option[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "is-flex is-justify-content-center")
+  end
 end
