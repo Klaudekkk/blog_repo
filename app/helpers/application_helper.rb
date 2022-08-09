4c984168c7e7acb325922ca1134ec8eb535d1c9f
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-09 13:54:00 +0200

    User show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     hash = Digest::MD5.hexdigest(email_adress)
     size = option[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "is-flex is-justify-content-center")
+    image_tag(gravatar_url, alt: user.username, class: " is-justify-content-center")
   end
 end
