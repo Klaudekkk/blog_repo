2cd5a1e8b2c195e394d022cdc9668c06f072a5b8
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-11 14:47:05 +0200

    Integration invalid form error

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -15,4 +15,17 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body #powinno wyswietlic sie sports i tego bedziemy szukac w body
   end
+
+  test "get new category form and reject invalid category submission" do #stworz nowa kategorie i odrzuć nieprawidłowe zgłoszenie
+    get "/categories/new" #uzyskanie ścieżki do nowej kategorii
+    assert_response :success # potwierdzenie że sie udało
+    #jeśli jest to nieprawidłowe zgłoszenie, nie powinno być różnicy w liczbie kategorii.
+    assert_no_difference 'Category.count'do
+      post categories_path, params: {category: {name: " "}} # to jest linia z kontrolera test z create
+      #jezeli tak sie stanie renderuje nam nowy form
+    end
+    assert_match "following errors", response.body #szukamy tych slow ktore maja sie znalezc w body
+    assert_select 'article.message' #shared/errors te dwie linijki sie tam znajduja i daja znać ze to chodzi o error
+    assert_select 'div.message-header'
+  end
 end
