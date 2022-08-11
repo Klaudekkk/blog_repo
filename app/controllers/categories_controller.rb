a287a353576eb45aa8fc87f3acc1d98947ca792f
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-11 12:29:43 +0200

    Categories new form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,25 @@ class CategoriesController < ApplicationController
 
   end
   def new
-
+    @category = Category.new
   end
 
   def show
 
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "categoria utowrzona"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
